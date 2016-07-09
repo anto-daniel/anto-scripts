eb42a0dbac274c4babb45345e7386880dd61a3e0
Puppet-Master <SAppini@actiance.com>
2016-07-09 16:46:44 +0530

    Modified Generic Script

--- generic.sh
+++ generic.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
-buildurl=$1
-builddir="/tmp/builddir"
+#echo -e "\nRemoving tmp files . . .\n" && rm -rf /tmp/*
+#buildurl=$1  ####  Example build url:  http://spider/data/ftbuilds/Alcatraz/feature-demo-integration/1.0.0-9913-gae2b77a/
+buildurl="http://spider/data/ftbuilds/Alcatraz/feature-demo-integration/1.0.0-9913-gae2b77a/"
+if [ ! $(echo $buildurl | grep "/$") ]
+then
+    echo -ne "\nurl does not ends with /\n"
+    buildurl=$(echo $buildurl | sed 's/$/\//') 
+fi
+buildnum=$(echo $buildurl |  awk -F'/' '{print $(NF-1)}')
+echo "Build Number: $buildnum"
+envname="jpmc"
+#envname=$2
+builddir="/tmp/$envname/build"
+finalbuild="/tmp/$envname/$buildnum"
 
-mkdir -p $builddir
+
+#if [[ $buildurl == "" || $envname == "" ]]
+#then
+#    echo "Usage: ./generic.sh <buildurl> <environment name>"
+#    exit 1
+#fi
+mkdir -p $builddir $finalbuild
+
 function validate_url() {
     if [[ `wget -S --spider $buildurl 2>&1 | grep 'HTTP/1.1 200 OK'` ]]
     then
-        echo -e "\nBuild URL:'$buildurl' is valid...\n"
+        echo -ne "\nBuild URL:'$buildurl' is valid...\n"
     else
         echo -e "\nInvalid $buildurl. Exiting from the program....\n"
         exit 1
@@ -15,98 +34,78 @@ function validate_url() {
 }
 
 function extract_url() {
-    wget -r --no-parent --no-directories --reject index.html* $buildurl -P $builddir/
+    echo -ne "\nExtracting Build URL in $builddir ...\n"
+    wget -r --no-parent --no-directories --reject index.html* $buildurl -P $builddir/ > /dev/null 2>&1
+    if [ $? -ne 0 ];then echo -e "\nDownloading Files from build url failed\n";exit 1;fi
 }
 
 function extract_karaf() {
+    echo -ne "\nStarting Karaf APC\n"
     cd $builddir
-    karaf_dir=$builddir/karaf_apc
-    karaf_file=$(ls -1 | grep karaf | grep -v exporter)
+    karaf_dir=$builddir/karaf_apc && mkdir -p $karaf_dir
+    final_karaf=$finalbuild/karaf && mkdir -p $final_karaf
+    karaf_file=$(ls -1 | egrep "karaf.*.tar.gz" | grep -v exporter | head -1)
     if [[ $(echo $karaf_file | egrep "tar.gz") ]]
     then
-        #echo "Its karaf archive file"
-        #echo $karaf_file
-        echo "Finding the karaf apc path"
+        echo -ne "\nFound karaf archive: $karaf_file\n"
     else
-        echo "karaf is not archive file"
-        exit 1
+        echo -ne "\nkaraf is not archive file: $karaf_file\n"
+        return 1
     fi
-    mkdir -p $karaf_dir 
-    sp_path=$(tar tf $karaf_file | grep "\/server.properties" 2>&1)
-    tar xvzf $karaf_file $sp_path -C $karaf_dir > /dev/null 2>&1
-    mv etc $karaf_dir
-    cd $karaf_dir
-    cwd=$(find . -name "server.properties" 2>&1)
-    ppwd="$(pwd)/$cwd"
-    apc_path=$(echo "$ppwd" | sed 's/\.\///')
-    echo -e "\nKARAF APC PATH => $apc_path \n"
+    sp_dir=$(tar tf $karaf_file | grep "\/server.properties" | sed -e 's/\/server\.properties//g' 2>&1)
+    echo -ne "\nExtracting Properties files in $karaf_dir\n"
+    tar xvzf $karaf_file -C ${karaf_dir} --wildcards "${sp_dir}/*.properties" > /dev/null 2>&1
+    if [ $? -ne 0 ];then echo -ne "\n Error while archiving karaf \n";return 1;fi
+    echo -ne "\nCopying Properties to builddir\n"
+    cp -rf ${karaf_dir}/${sp_dir}/*.properties $final_karaf
+    if [ $? -eq 0 ];then echo -ne "\nKaraf APC done\n";else echo -ne "\nError While copying\n";fi 
 }
 
 function extract_nfs() {
+    echo -ne "\nStarting Karaf NFS\n"
     cd $builddir
-    nfs_dir=$builddir/karaf_nfs
-    karaf_file=$(ls -1 | grep karaf | grep exporter)
-    if [[ $(echo $karaf_file | egrep "tar.gz") ]]
+    nfs_dir=$builddir/karaf_nfs && mkdir -p $nfs_dir
+    final_nfs=$finalbuild/karaf_nfs && mkdir -p $final_nfs
+    nfs_file=$(ls -1 | egrep "karaf.*.tar.gz" | grep exporter | head -1)
+    if [[ $(echo $nfs_file | egrep "tar.gz$") ]]
     then
-        #echo "Its nfs archive file"
-        #echo $karaf_file
-        echo "Finding the karaf NFS path"
+        echo -ne "\nFound the karaf NFS archive: $nfs_file\n"
     else
-        echo "karaf nfs is not archive file"
-        exit 1
+        echo -ne "\nkaraf nfs is not archive file: $nfs_file\n"
+        return 1
     fi
-    mkdir -p $nfs_dir
-    sp_path=$(tar tf $karaf_file | grep "\/server.properties" 2>&1)
-    tar xvzf $karaf_file $sp_path -C $nfs_dir > /dev/null 2>&1
-    mv etc $nfs_dir
-    cd $nfs_dir
-    cwd=$(find . -name "server.properties" 2>&1)
-    ppwd="$(pwd)/$cwd"
-    apc_path=$(echo "$ppwd" | sed 's/\.\///')
-    echo -e "\nKARAF NFS PATH => $apc_path \n"
+    sp_dir=$(tar tf $nfs_file | grep "\/server.properties" | sed -e 's/\/server\.properties//g' 2>&1)
+    echo -e "\nExtracting Properties files in $nfs_dir"
+    tar xvzf $nfs_file -C ${nfs_dir} --wildcards "${sp_dir}/*.properties" > /dev/null 2>&1
+    if [ $? -ne 0 ];then echo -e "\n Error while archiving karaf nfs \n";return 1;fi
+    echo -e "\nCopying Properties to builddir\n"
+    cp -rf ${nfs_dir}/${sp_dir}/*.properties $final_nfs
+    if [ $? -eq 0 ];then echo -ne "\nKaraf NFS done\n";else echo -ne "\nKaraf NFS: Error While copying\n";fi 
 }
 
 function extract_hazelcast() {
+    echo -ne "\nStarting Hazelcast\n"
     cd $builddir
-    hz_dir=$builddir/hazelcast
-    hazel_archive=$(ls -1 | egrep "alcatraz_cache.*.zip")
-    #echo "$hazel_archive"
-    hz_archive_path=$(unzip -l $hazel_archive | grep server.properties | awk '{print $4}' 2>&1)
-    mkdir -p $hz_dir 
-    unzip -j $hazel_archive $hz_archive_path -d $hz_dir > /dev/null 2>&1
-    if [[ $(ls $hz_dir/server.properties) ]]
-    then
-        echo -e "\n HAZELCAST BUILD Path => $(ls $hz_dir/server.properties)"
-    else
-        echo "hazelcast build path"
-        find $hz_dir -name "server.properties"
-    fi
-        
+    final_hz=$finalbuild/hazel && mkdir -p $final_hz && rm -rf $final_hz/*
+    hazel_archive=$(ls -1 | egrep "alcatraz_cache.*.zip$" | head -1)
+    unzip -j $hazel_archive "*.properties" -d $final_hz > /dev/null 2>&1
+    if [ $? -eq 0 ];then echo -ne "\nHazel done\n";else echo -ne "\nHazel: Error While archiving\n";fi 
 }
 
 function extract_storm() {
+    echo -ne "\nStarting Storm\n"
     cd $builddir
-    stm_dir=$builddir/storm
-    stm_archive=$(ls -1 | egrep "storm.*.zip")
-    #echo "$stm_archive"
-    stm_archive_path=$(unzip -l $stm_archive | grep server.properties | awk '{print $4}' 2>&1)
-    mkdir -p $stm_dir 
-    unzip -j $stm_archive $stm_archive_path -d $stm_dir > /dev/null 2>&1
-    if [[ $(ls $stm_dir/server.properties) ]]
-    then
-        echo -e "\n STORM BUILD Path => $(ls $stm_dir/server.properties)"
-    else
-        echo "hazelcast build path"
-        find $hz_dir -name "server.properties"
-    fi
-        
+    final_stm=$finalbuild/storm && mkdir -p $final_stm && rm -rf $final_stm/*
+    stm_archive=$(ls -1 | egrep "storm.*.zip$" | head -1)
+    unzip -j $stm_archive "*.properties" -d $final_stm > /dev/null 2>&1
+    if [ $? -eq 0 ];then echo -ne "\nStorm done\n";else echo -ne "\nStorm: Error While archiving\n";fi 
 }
 
 
 
 
 validate_url
-extract_url
+#extract_url
 extract_karaf
 extract_nfs
 extract_hazelcast
