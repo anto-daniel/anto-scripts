fab85dbdb21cf817191ffe7fc7c06e82d63fd829
Anto Daniel Joseph Paul <APaul@actiance.com>
2016-07-11 01:41:02 +0530

    fab deployed

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+hostfile=${1}
+commands_list=${2}
+user=apcuser
+pass=facetime
+if [[ $hostfile == "" || $commands_list == "" ]]
+then
+    echo "Usage: ./run_cmd.sh <host list file> <commands list file>"
+    exit 1
+fi
+
+while read host
+do
+        echo -ne "\n$host\n"
+        while read -r cmd
+        do
+            sshpass -p $pass ssh -n -o StrictHostKeyChecking=no $host  "$cmd" 
+        done < $commands_list
+done < $hostfile
