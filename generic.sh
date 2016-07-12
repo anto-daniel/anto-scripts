956534eb8e43f60a35371a33eddad08d933d13e0
Anto Daniel Joseph Paul <APaul@actiance.com>
2016-07-12 06:27:38 +0530

    fab deployed

--- generic.sh
+++ generic.sh
@@ -8,18 +8,18 @@ then
     buildurl=$(echo $buildurl | sed 's/$/\//') ###    echo -ne "\nurl does not ends with /\n"
 fi
 buildnum=$(echo $buildurl |  awk -F'/' '{print $(NF-1)}')
-echo -ne "\nBuild Number: $buildnum"
 envname=$2
 builddir="/tmp/$envname/build"
 finalbuild="/tmp/$envname/$buildnum"
 
+echo -ne "\nBuild Number: $buildnum"
+mkdir -p $builddir $finalbuild
 
 if [[ $buildurl == "" || $envname == "" ]]
 then
     echo "Usage: ./generic.sh <buildurl> <environment name>"
     exit 1
 fi
-mkdir -p $builddir $finalbuild
 
 function validate_url() {
     echo -ne "\nValidating BuildURL: $buildurl\n"
@@ -35,7 +35,11 @@ function validate_url() {
 function extract_url() {
     echo -ne "\nExtracting build in $builddir ...\n"
     wget -r --no-parent --no-directories --reject index.html* $buildurl -P $builddir/ > /dev/null 2>&1
-    if [ $? -ne 0 ];then echo -e "\nDownloading Files from build url failed\n";exit 1;fi
+    if [ $? -ne 0 ]
+	then
+		echo -e "\nDownloading Files from build url failed\n"
+		exit 1
+	fi
     echo -ne "Extraction Done.\n"
 }
 
@@ -55,10 +59,19 @@ function extract_karaf() {
     sp_dir=$(tar tf $karaf_file | grep "\/server.properties" | sed -e 's/\/server\.properties//g' 2>&1)
     echo -ne "Extracting Properties files in $karaf_dir\n"
     tar xvzf $karaf_file -C ${karaf_dir} --wildcards "${sp_dir}/*.properties" > /dev/null 2>&1
-    if [ $? -ne 0 ];then echo -ne "Error while archiving karaf \n";return 1;fi
+    if [ $? -ne 0 ]
+	then
+		echo -ne "Error while archiving karaf \n"
+		return 1
+	fi
     echo -ne "Copying Properties to $final_karaf\n"
     cp -rf ${karaf_dir}/${sp_dir}/*.properties $final_karaf
-    if [ $? -eq 0 ];then echo -ne "Karaf APC done\n";else echo -ne "Error While copying\n";fi 
+    if [ $? -eq 0 ]
+	then
+		echo -ne "Karaf APC done\n"
+	else
+		echo -ne "Karaf: Error While copying\n"
+	fi 
 }
 
 function extract_nfs() {
@@ -77,10 +90,19 @@ function extract_nfs() {
     sp_dir=$(tar tf $nfs_file | grep "\/server.properties" | sed -e 's/\/server\.properties//g' 2>&1)
     echo -ne "Extracting Properties files in $nfs_dir\n"
     tar xvzf $nfs_file -C ${nfs_dir} --wildcards "${sp_dir}/*.properties" > /dev/null 2>&1
-    if [ $? -ne 0 ];then echo -e "Error while archiving karaf nfs \n";return 1;fi
-    echo -ne "Copying Properties to $final_nfs\n"
+    if [ $? -ne 0 ]
+	then
+		echo -e "Error while archiving karaf nfs \n"
+		return 1
+	fi
+	echo -ne "Copying Properties to $final_nfs\n"
     cp -rf ${nfs_dir}/${sp_dir}/*.properties $final_nfs
-    if [ $? -eq 0 ];then echo -ne "Karaf NFS done\n";else echo -ne "Karaf NFS: Error While copying\n";fi 
+    if [ $? -eq 0 ]
+	then 
+		echo -ne "Karaf NFS done\n"
+	else
+		echo -ne "Karaf NFS: Error While copying\n"
+	fi 
 }
 
 function extract_hazelcast() {
@@ -89,7 +111,12 @@ function extract_hazelcast() {
     final_hz=$finalbuild/build/hazel && mkdir -p $final_hz && rm -rf $final_hz/*
     hazel_archive=$(ls -1 | egrep "alcatraz_cache.*.zip$" | head -1)
     unzip -j $hazel_archive "*.properties" -d $final_hz > /dev/null 2>&1
-    if [ $? -eq 0 ];then echo -ne "Hazel done\n";else echo -ne "Hazel: Error While archiving\n";fi 
+    if [ $? -eq 0 ]
+	then
+		echo -ne "Hazel done\n"
+	else 
+		echo -ne "Hazel: Error While archiving\n"
+	fi 
 }
 
 function extract_storm() {
@@ -98,7 +125,12 @@ function extract_storm() {
     final_stm=$finalbuild/build/storm && mkdir -p $final_stm && rm -rf $final_stm/*
     stm_archive=$(ls -1 | egrep "storm.*.zip$" | head -1)
     unzip -j $stm_archive "*.properties" -d $final_stm > /dev/null 2>&1
-    if [ $? -eq 0 ];then echo -ne "Storm done\n";else echo -ne "Storm: Error While archiving\n";fi 
+    if [ $? -eq 0 ]
+	then
+		echo -ne "Storm done\n"
+	else 
+		echo -ne "Storm: Error While archiving\n"
+	fi 
 }
 
 function copy_karaf_production() {
@@ -106,6 +138,10 @@ function copy_karaf_production() {
     karaf_prod_dir="/etc/puppet/environments/production/modules/karaf_birthing/templates"
     cd $karaf_prod_dir && cp -rf *.properties.erb $karaf_build_dir
     if [ $? -ne 0 ];then echo -ne "\n Property erb files missing in $karaf_prod_dir\n";return 1;fi
+    then 
+        echo -ne "\n Property erb files missing in $karaf_prod_dir\n"
+        return 1
+    fi
     cd $karaf_build_dir
     for file in $(ls -1 | egrep  ".*.properties.erb$")
     do
@@ -118,7 +154,6 @@ function copy_nfs_production() {
     nfs_build_dir="${finalbuild}/production/karaf_nfs" && mkdir -p ${nfs_build_dir}
     nfs_prod_dir="/etc/puppet/environments/production/modules/karaf_nfs_birthing/templates"
     cd $nfs_prod_dir && cp -rf *.properties.erb $nfs_build_dir
-    if [ $? -ne 0 ];then echo -ne "\nProperty erb files missing in $nfs_prod_dir\n";return 1;fi
     cd $nfs_build_dir
     for file in $(ls -1 | egrep  ".*.properties.erb$")
     do
@@ -132,6 +167,10 @@ function copy_hz_production() {
     hz_prod_dir="/etc/puppet/environments/production/modules/hazelcast_birthing/templates"
     cd $hz_prod_dir && cp -rf *.properties.erb $hz_build_dir
     if [ $? -ne 0 ];then echo -ne "\nProperty erb files missing in $hz_prod_dir";return 1;fi
+    then 
+        echo -ne "\nProperty erb files missing in $hz_prod_dir"
+        return 1
+    fi
     cd $hz_build_dir
     for file in $(ls -1 | egrep  ".*.properties.erb$")
     do
@@ -145,6 +184,10 @@ function copy_stm_production() {
     stm_prod_dir="/etc/puppet/environments/production/modules/storm_birthing/templates"
     cd $stm_prod_dir && cp -rf *.properties.erb $stm_build_dir
     if [ $? -ne 0 ];then echo -ne "\n Property erb files missing in $stm_prod_dir\n";return 1;fi
+    then 
+        echo -ne "\n Property erb files missing in $stm_prod_dir\n"
+        return 1
+    fi
     cd $stm_build_dir
     for file in $(ls -1 | egrep  ".*.properties.erb$")
     do
@@ -168,5 +211,6 @@ copy_stm_production
 
 
 echo -ne "\nRemoving tmp build files\n" && rm -rf $builddir/*
+echo -ne "\nCreating Merged directories in $finalbuild\n"
 mkdir -p $finalbuild/merged/{hazel,karaf,karaf_nfs,storm}
 tree $finalbuild
