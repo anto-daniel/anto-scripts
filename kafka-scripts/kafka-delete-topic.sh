95fe091f427534de80f2bad4a28d2c208c30a629
Anto Daniel <anto.daniel@gmail.com>
2019-07-31 20:26:28 +0530

    correction in kafka deletion script

--- kafka-scripts/kafka-delete-topic.sh
+++ kafka-scripts/kafka-delete-topic.sh
@@ -4,6 +4,6 @@ zk_bin=/apps/zookeeper/bin
 if [ $# -ne 1 ]; then echo topic name in 1st arg;exit 1;fi
 topic=$1
 
-echo "rmr /brokers/topics/$topic | $zk_bin/zkCli.sh"
+echo "rmr /brokers/topics/$topic" | $zk_bin/zkCli.sh
 if [ $? -ne 0 ]; then echo unable to delete topic;exit 1;fi
 echo "Succesfully deleted the topic $topic"
